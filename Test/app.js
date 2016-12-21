0339ab10eddddc806bc08b6d749189607792b7d1
wassgha <wassgha@gmail.com>
2016-12-21 12:40:58 -0500

    Fixed widget

--- Test/app.js
+++ Test/app.js
@@ -5,14 +5,5 @@ const ipc = require('electron').ipcRenderer
 
 $(function(){
 
-  const box = $('#box')
-  box.draggable()
-  // box.on('click', (event) => {
-  //   ipc.send('new_win', {
-  //     width: 250,
-  //     height: 350,
-  //     frame: false,
-  //     resizable: false
-  //   })
-  // })
+  $('.widget').draggable();
 })
