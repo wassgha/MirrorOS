363121afb97f1110db3ef2ffb7a9c36665691288
wassgha <wassgha@gmail.com>
2016-12-22 14:35:33 -0500

    Hidden curson

--- app.js
+++ app.js
@@ -12,44 +12,7 @@ $(document).ready(function () {
       $(this).css('z-index', 1);;
     });
 
-    $("#content").load("login.html", function() {
-      // Show camera feed in login widget
-      var video = document.querySelector(".login.widget #cam");
-      var localstream;
-      navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia || navigator.oGetUserMedia;
-       
-      if (navigator.getUserMedia) {       
-          navigator.getUserMedia({video: true}, handleVideo, videoError);
-      }
-       
-      function handleVideo(stream) {
-        localstream = stream;
-        video.src = window.URL.createObjectURL(stream);
-      }
-
-      function videoError(e) {
-          // do something
-      }
-
-      // If face recognized then login
-      $(".login.widget").on("click", function () {
-        $(".login.widget").fadeOut(500);
-        $(".loader").fadeIn(1000, function() {
-          setTimeout(function() {
-            $(".notification").fadeIn(1000);
-            setTimeout(function() { 
-              $(".loader").fadeOut();
-              setTimeout(function() { 
-                $("#content").load("home.html", function () {
-                  $(".notification").fadeOut(1000);
-                  localstream.getTracks()[0].stop();
-                });
-              }, 500);
-            }, 1000);
-          }, 500);
-        });
-      });
-    });
+    $("#content").load("login.html");
 });
 
 $(document).ajaxComplete(function () {
