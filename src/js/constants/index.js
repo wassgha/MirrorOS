947865bee5e265f14a8a78aba6bb03f0ed438a38
wassgha <wassgha@gmail.com>
2017-07-22 20:35:54 -0700

    Refactor

---
+++ src/js/constants/index.js
@@ -0,0 +1,20 @@
+export const INITIALIZE_QUEUE = 'INITIALIZE_QUEUE'
+export const ADD_TO_QUEUE = 'ADD_TO_QUEUE'
+export const REMOVE_FROM_QUEUE = 'REMOVE_FROM_QUEUE'
+
+export const CREATE_WIDGET = 'CREATE_WIDGET'
+export const REMOVE_WIDGET = 'REMOVE_WIDGET'
+
+export const INITIALIZE_DATE_AND_TIME = 'INITIALIZE_DATE_AND_TIME'
+export const UPDATE_DATE_AND_TIME = 'UPDATE_DATE_AND_TIME'
+
+export const LOAD_WEATHER = 'LOAD_WEATHER'
+export const OPEN_WEATHER_MAP_KEY = '7f66f4d6e94b9ccafd4096de6f52a6a1'
+export const OPEN_WEATHER_MAP_ADDRESS = 'http://api.openweathermap.org/data/2.5/weather'
+
+export const LOAD_POSITION = 'LOAD_POSITION'
+export const LOAD_FORMATTED_ADDRESS = 'LOAD_FORMATTED_ADDRESS'
+export const GOOGLE_GEOCODING_ADDRESS = 'https://maps.googleapis.com/maps/api/geocode/json'
+export const GOOGLE_API_KEY = 'AIzaSyAxEd1c2fuK7zBlHV6ENZ1Ua2uqfP1Yfl8'
+
+export const GENERATE_SOCKET = 'GENERATE_SOCKET'
